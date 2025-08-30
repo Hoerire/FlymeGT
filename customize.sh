7776b7c5573fc4bc97b3787c1604aee9d0c8cf49
Kari <128872549+Hoerire@users.noreply.github.com>
2025-08-30 16:35:20 +0800

    创建 customize.sh

---
+++ customize.sh
@@ -0,0 +1,27 @@
+SKIPMOUNT=false
+PROPFILE=true
+POSTFSDATA=true
+LATESTARTSERVICE=true
+REPLACE=""
+
+# 模块信息
+id=Flyme_GT
+name="电池信息展示&优化温控限制"
+version=V1.0.0
+versionCode=1
+author=Kari
+description="Flyme_GT"
+
+# 生成module.prop
+echo "id=$id
+name=$name
+version=$version
+versionCode=$versionCode
+author=$author
+description=$description" > $MODPATH/module.prop
+
+# 安装提示
+ui_print "模块: $name"
+ui_print "版本: $version"
+ui_print "安装完成"
+ui_print "点击小三角执行刷新电池信息"
